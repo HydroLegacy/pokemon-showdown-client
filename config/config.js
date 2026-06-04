1150a2f8b3bbb501356543f52bde579f5456a469
HydroLegacy <hydrolegacygd1@gmail.com>
2026-06-03 21:28:02 -0400

    Update and rename config-example.js to config.js

--- config/config.js
+++ config/config.js
@@ -19,9 +19,9 @@ Config.whitelist = [
 // `defaultserver` specifies the server to use when the domain name in the
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
-	id: 'showdown',
-	host: 'sim3.psim.us',
-	port: 443,
+	id: 'showdowntestthingy',
+	host: 'localhost',
+	port: 8000,
 	httpport: 8000,
 	altport: 80,
 	registered: true
